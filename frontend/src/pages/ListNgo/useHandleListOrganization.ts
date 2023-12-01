8a1d651e89da0d400e08ebdeb2f231f8f367ec0c
nandodsb <nandosbx@gmail.com>
2023-12-01 00:02:17 -0300

    Returning Organizations

--- frontend/src/pages/ListNgo/useHandleListOrganization.ts
+++ frontend/src/pages/ListNgo/useHandleListOrganization.ts
@@ -1,1 +1,15 @@
-export default async function useHandleListOrganization() {}
+import { organizationState } from '@/recoil';
+import api from '@/services/api';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+
+export default async function useHandleListOrganization() {
+	const [organization, setOrganization] = useRecoilState(organizationState);
+
+	useEffect(() => {
+		api.get('/ngos').then((response) => {
+			setOrganization(response.data);
+		});
+	}, [organization]);
+	return { organization, setOrganization };
+}
